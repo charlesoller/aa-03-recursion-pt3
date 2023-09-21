b20ee2523cc84f65a356ea18a36cb93ae952f310
Javier Hernandez <javierLhernandez@yahoo.com>
2023-09-21 19:20:47 -0400

    11

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -14,8 +14,41 @@ flatten([1, [2, [3]]]); // [1, 2, 3]
 
 function flatten(arr) {
   // Your code here 
+  // base case
+  // if(!arr.length) {
+  //   return arr
+  // }
+
+  // let currArr = []
+  // // recursive step
+  // // let test = arr.pop()
+  // if(Array.isArray(arr[0])) {
+  //   currArr.push(...flatten(arr.shift()))
+  // }
+
+  //recursive case
+  // return currArr
+
+  const flat = [];
+
+  arr.forEach(item => {
+    if (Array.isArray(item)){ //base
+      flat.push(...flatten(item)) //recursive
+    } else { //base
+      flat.push(item);
+    }
+  })
+
+  return flat
 }
-  
+
+// 
+// 1 , f(arr)
+
+
+console.log(flatten([])); // []
+console.log(flatten([1, 2])); // [1, 2]
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
   
