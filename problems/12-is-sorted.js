0b92f8fc4258e326db1ad73624e464bb487afcf6
charlesoller <charlesrello@gmail.com>
2023-09-21 18:17:25 -0400

    1/4 problems done (12)

--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -12,9 +12,29 @@ isSorted([5, 4, 3, 2, 1]); // false
 
 
 function isSorted(arr) {
-  // Your code here 
+
+  // //base case
+  // if (arr.length === 1) return true;
+
+  // //recursive step
+  // let first = arr.shift();
+  // if (first > arr[0]) return false;
+
+  // //recursive case
+  // return isSorted(arr);
+
+
+  if (arr.length === 1) return true;
+  if (arr.shift() > arr[0]) return false;
+  return isSorted(arr);
+
+
 }
 
+console.log(isSorted([1, 2, 3, 4, 5])); // true
+console.log(isSorted([1, 2, 4, 3, 5])); // false
+console.log(isSorted([2, 4, 6, 7, 8])); // true
+console.log(isSorted([5, 4, 3, 2, 1])); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
